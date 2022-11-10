f18e33609e9f0090a9ff1b734252e15514feb746
sarki012 <60797267+sarki012@users.noreply.github.com>
2022-11-10 13:10:33 -0600

    Servo Starts and Stops Instantaneously

--- Robotic Arm/boomThread.c
+++ Robotic Arm/boomThread.c
@@ -18,7 +18,7 @@ volatile extern char usbRxval[5];     //The UART receive array which holds the d
 void boomThread( void *pvParameters )
 {
     int  i = 0, direction = 1;
-    int numDelayLoops = 1000;
+    int numDelayLoops = 500;
  
     PHASE2 = 36850;         //PHASEx is always 36,850 for a 50Hz pulse
     PDC2 = 3870;            //Duty cycle register. Starting duty cycle is x. Max + PDCx = 1658, max - PDCx = 3870
@@ -26,27 +26,25 @@ void boomThread( void *pvParameters )
     {
         for(i = 0; i < 5; i++)
         {
-            if(usbRxval[i] != '!' && usbRxval[i] != '@')
+            if(usbRxval[i] == 'u')
             {
-                if(usbRxval[i] == 'u')
+                PDC2 --;         //Decrementing the duty cycle moves the stick out
+                delay(numDelayLoops);
+                if(PDC2 < 1658)
                 {
-                    PDC2 -= 5;         //Decrementing the duty cycle moves the stick out
-                    delay(numDelayLoops);
-                    if(PDC2 < 1658)
-                    {
-                        PDC2 = 1658;        //We don't let PDC2 get less than 1658
-                    }
-                }
-                else if(usbRxval[i] == 'd')
-                {
-                    PDC2 += 5;         //Incrementing the duty cycle moves the stick in
-                    delay(numDelayLoops);
-                    if(PDC2 > 3870)
-                    {
-                        PDC2 = 3870;        //We don't let PDC2 get greater than 3870
-                    } 
+                    PDC2 = 1658;        //We don't let PDC2 get less than 1658
                 }
             }
+            else if(usbRxval[i] == 'd')
+            {
+                PDC2 ++;         //Incrementing the duty cycle moves the stick in
+                delay(numDelayLoops);
+                if(PDC2 > 3870)
+                {
+                    PDC2 = 3870;        //We don't let PDC2 get greater than 3870
+                } 
+            }
+            
         }
     }
 }
