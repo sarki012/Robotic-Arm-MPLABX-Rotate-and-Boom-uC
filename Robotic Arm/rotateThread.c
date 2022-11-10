d6719ba79ddbdecd6c845cb18aaee9343915f34f
sarki012 <60797267+sarki012@users.noreply.github.com>
2022-11-10 13:02:14 -0600

    Stepper Starts and Stops Instantaneously

--- Robotic Arm/rotateThread.c
+++ Robotic Arm/rotateThread.c
@@ -11,19 +11,19 @@
 #include "FreeRTOSConfig.h"
 #include "main.h"
 
-volatile extern char usbRxval[50];     //The UART receive array which holds the data sent 
+volatile extern char usbRxval[5];     //The UART receive array which holds the data sent 
                                     //via Bluetooth from the tablet
 void rotateThread( void *pvParameters )
 {
     int rightFlag = 0, leftFlag = 0;
     LATAbits.LATA4 = 0;     //Enable (0 = On)
-  // LATBbits.LATB0 = 1;     //Direction
+  //  LATBbits.LATB0 = 1;     //Direction
     //PHASE1 = 10000;
     //PDC1 = PHASE1/2;
     int  i = 0;
     while(1)
     {
-      for(i = 0; i < 45; i++)
+      for(i = 0; i < 5; i++)
         {
             if(usbRxval[i] == 'r')
             {
@@ -31,6 +31,7 @@ void rotateThread( void *pvParameters )
                 LATBbits.LATB0 = 1;     //Direction
                 PHASE1 = 10000;
                 PDC1 = PHASE1/2;
+                rightFlag = 1;
             }
             else if(usbRxval[i] == 'l')
             {
@@ -38,7 +39,18 @@ void rotateThread( void *pvParameters )
                 LATBbits.LATB0 = 0;     //Direction
                 PHASE1 = 10000;
                 PDC1 = PHASE1/2;
+                leftFlag = 1;
             }
+            if(i == 4){
+                if(rightFlag == 0 && leftFlag == 0)
+                {
+                    PHASE1 = 6000;
+                    PDC1 = 0;
+                }
+                rightFlag = 0;
+                leftFlag = 0;
+            }
+            /*
             else if(usbRxval[i] == '#')
             {
                leftFlag = 1; 
@@ -47,6 +59,8 @@ void rotateThread( void *pvParameters )
             {
                rightFlag = 1; 
             }
+             * */
+            /*
             if(leftFlag == 1 && rightFlag == 1)
             {
                 leftFlag = 0;
@@ -54,7 +68,7 @@ void rotateThread( void *pvParameters )
                 PHASE1 = 6000;
                 PDC1 = 0;
                 break;
-            }
+            }*/
         }
            
     }
